8898b8f8feddd1cb64408582d6b39665c094490c
okellogeorgeokoth <okoth603@gmail.com>
2022-02-19 17:54:33 +0300

    Duplicate Elimination Task completed

---
+++ Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.inputBox = New System.Windows.Forms.TextBox()
+        Me.submitBtn = New System.Windows.Forms.Button()
+        Me.clearBtn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.numberEnteredListBox = New System.Windows.Forms.ListBox()
+        Me.uniqueValuesListBox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Number:"
+        '
+        'inputBox
+        '
+        Me.inputBox.Location = New System.Drawing.Point(36, 60)
+        Me.inputBox.Name = "inputBox"
+        Me.inputBox.Size = New System.Drawing.Size(100, 23)
+        Me.inputBox.TabIndex = 1
+        '
+        'submitBtn
+        '
+        Me.submitBtn.Location = New System.Drawing.Point(36, 103)
+        Me.submitBtn.Name = "submitBtn"
+        Me.submitBtn.Size = New System.Drawing.Size(100, 23)
+        Me.submitBtn.TabIndex = 2
+        Me.submitBtn.Text = "Submit"
+        Me.submitBtn.UseVisualStyleBackColor = True
+        '
+        'clearBtn
+        '
+        Me.clearBtn.Location = New System.Drawing.Point(36, 141)
+        Me.clearBtn.Name = "clearBtn"
+        Me.clearBtn.Size = New System.Drawing.Size(100, 23)
+        Me.clearBtn.TabIndex = 3
+        Me.clearBtn.Text = "Clear Data"
+        Me.clearBtn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(170, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Numbers:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(337, 30)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Unique Numbers:"
+        '
+        'numberEnteredListBox
+        '
+        Me.numberEnteredListBox.FormattingEnabled = True
+        Me.numberEnteredListBox.ItemHeight = 15
+        Me.numberEnteredListBox.Location = New System.Drawing.Point(173, 60)
+        Me.numberEnteredListBox.Name = "numberEnteredListBox"
+        Me.numberEnteredListBox.Size = New System.Drawing.Size(131, 349)
+        Me.numberEnteredListBox.TabIndex = 6
+        '
+        'uniqueValuesListBox
+        '
+        Me.uniqueValuesListBox.FormattingEnabled = True
+        Me.uniqueValuesListBox.ItemHeight = 15
+        Me.uniqueValuesListBox.Location = New System.Drawing.Point(340, 60)
+        Me.uniqueValuesListBox.Name = "uniqueValuesListBox"
+        Me.uniqueValuesListBox.Size = New System.Drawing.Size(128, 349)
+        Me.uniqueValuesListBox.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(496, 425)
+        Me.Controls.Add(Me.uniqueValuesListBox)
+        Me.Controls.Add(Me.numberEnteredListBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.clearBtn)
+        Me.Controls.Add(Me.submitBtn)
+        Me.Controls.Add(Me.inputBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Duplicate Elimination"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents inputBox As TextBox
+    Friend WithEvents submitBtn As Button
+    Friend WithEvents clearBtn As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents numberEnteredListBox As ListBox
+    Friend WithEvents uniqueValuesListBox As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,117 @@
+﻿'Name
+
+'Details
+Public Class Form1
+    'num and vcount variables  hold value and count
+
+    Dim num, vcount As Integer
+
+    'array holding 20 values
+
+    Dim values(19) As Integer
+
+    'count variable set to 0
+
+    Dim count As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    'submt button to input the value and disply the value Numbers and Unique Numbers
+
+    Private Sub submitBtn_Click(sender As Object, e As EventArgs) Handles submitBtn.Click
+        'getting the value to num variable
+        num = Val(inputBox.Text)
+
+        'checking for value from 10 to 100
+
+        If num >= 10 And num <= 100 Then
+
+            'add  value to list box
+
+            numberEnteredListBox.Items.Add(num)
+
+            'vcount increases the value count
+
+            vcount += 1
+
+            'if count equals to 20 then
+
+            If (vcount.Equals(20)) Then
+
+                'disable submit button
+
+                submitBtn.Enabled = False
+
+                'enable clear button
+
+                clearBtn.Enabled = True
+
+            End If
+
+            'function used to find unique values
+
+            findUniqueValues(num)
+
+            'if value entered is less than 10
+
+        ElseIf num < 10 Then
+            'if value entered is less than 10
+
+            MsgBox("Value is less than 10", , "Error")
+            'if value entered is greater than 100
+
+        ElseIf num > 100 Then
+
+            'display error message
+
+            MsgBox("Value is greater than 100", , "Error")
+
+        End If
+    End Sub
+    'clears the input box for next value 
+    Private Sub clearBtn_Click(sender As Object, e As EventArgs) Handles clearBtn.Click
+        inputBox.Text = ""
+    End Sub
+    'function used to find the unique values 
+
+    Sub findUniqueValues(n As Integer)
+
+        'variable to hold status for unique value
+
+        Dim f As Integer = 0
+
+        'Loop used to find the next value if available
+
+        For i = 0 To values.GetUpperBound(0)
+            'if yes
+            If (values(i).Equals(n)) Then
+
+                'set flag to 1
+
+                f = 1
+
+            End If
+
+        Next
+
+        'if flag is equal to 0
+
+        If f.Equals(0) Then
+
+            'Store the unique values
+
+            values(count) = n
+
+            'Display the unique values to unique value list box
+
+            uniqueValuesListBox.Items.Add(values(count))
+
+            'Increases unique values count  
+
+            count += 1
+
+        End If
+
+    End Sub
+End Class
